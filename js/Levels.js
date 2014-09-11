e8668d12b79abaa127850df805b01f11b9217066
George Zeng <george.x.zeng@gmail.com>
2014-09-11 15:13:20 -0400

    modified level selectors

--- js/Levels.js
+++ js/Levels.js
@@ -2,7 +2,7 @@ Levels = function(game) {};
 
 Levels.prototype = {
 	create: function() {
-    this.add.sprite(55, 45, 'stageSelect');
+    this.add.sprite(150, 60, 'stageSelect');
 		this.startButton1 = this.add.button(50, 133, 'map1', this.stageOne, this, 1, 0, 2);
     this.startButton2 = this.add.button(415, 183, 'map2', this.stageTwo, this, 1, 0, 2);
     this.startButton3 = this.add.button(50, 383, 'map3', this.stageThree, this, 1, 0, 2);
