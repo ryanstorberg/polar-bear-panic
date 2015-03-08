8e2d8f564a50d5db150e2916632a6eaf509d2af3
Ryan Storberg <ryan@storberg.net>
2015-03-07 22:54:49 -0500

    Change port configuration

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.get("/assets/*", function(req, res) {
   res.sendfile(file);
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
     console.log("Listening on port %d", server.address().port);
 });
