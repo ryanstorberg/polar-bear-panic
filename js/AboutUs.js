502253ab279b3f1c920ec9864569439f3849dc5a
Ryan Storberg <ryan@storberg.net>
2015-03-08 13:39:54 -0400

    Delete unused game file

--- js/AboutUs.js
+++ js/AboutUs.js
@@ -5,7 +5,7 @@ AboutUs.prototype = {
 		this.spacebar = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
 		label = this.game.add.text(100, 150, 
-			'Our names are Alex, Derek, Garrett, \n George, and Ryan.  We worked on and completed\n Polar Bear Panic over a week and a half\n period for our\n teams final project at Dev Boot Camp \n Thank you for playing our game!\n\n\n Level music by Eric Skiff\n\n\nPress SPACEBAR to return to the menu',{ fill: '#fff', align: 'center'});
+			'Our names are Alex, Derek, Garrett, \n George, and Ryan.  We worked on and completed\n Polar Bear Panic over a week and a half\n period for our\n teams final project at Dev Bootcamp \n Thank you for playing our game!\n\n\n Level music by Eric Skiff\n\n\nPress SPACEBAR to return to the menu',{ fill: '#fff', align: 'center'});
 		},
 
 	update: function() {
